8ac535f3f0cf89b87883025eb03e29220a93a71d
Usuario <Usuario@DESKTOP-BIRK270>
2022-08-28 10:25:45 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace Ejercicio_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            saludar("Fabio");
+        }
+
+        public static void saludar(string nombre)
+        {
+            Console.WriteLine("Hola" + nombre);
+        }
+    }
+
+}
